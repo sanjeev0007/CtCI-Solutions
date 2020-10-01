364b8a47251e4bc46b468d01931d71bd4d2d5c64
sanjeev0007 <sonisanjeev650@gmail.com>
2020-10-01 18:41:46 +0530

    new

---
+++ Arrays and Strings/oneAway.cpp
@@ -0,0 +1,44 @@
+/*
+
+Problem Statement!!!
+
+There are three types of edits that can be performed on strings: insert a character,
+remove a character, or replace a character. Given two strings, write a function to check if they are
+one edit (or zero edits) away
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Approach 1
+
+bool oneAway(string s1, string s2)
+{
+    map<int, int> mp1;
+    for (int i = 0; i < s1.size(); ++i)
+    {
+        mp1[s1[i]]++;
+    }
+    int ct = 0;
+    for (int i = 0; i < s2.size(); ++i)
+    {
+        mp1[s2[i]]--;
+        if (mp1[s2[i]] < 0)
+            ct++;
+    }
+    return ct <= 1;
+}
+
+int main()
+{
+    string s1, s2;
+    cin >> s1 >> s2;
+    int n1 = s1.size();
+    int n2 = s2.size();
+    if (n1 - n2 <= 1)
+        cout << oneAway(s1, s2) << "\n";
+    else
+        cout << "false"
+             << "\n";
+}
---
+++ Arrays and Strings/stringComp.cpp
@@ -0,0 +1,26 @@
+/*
+
+Problem Statement!!!
+
+Implement a method to perform basic string compression using the counts
+of repeated characters. For example, the string aabcccccaaa would become a2blc5a3. If the
+"compressed" string would not become smaller than the original string, your method should return
+the original string. You can assume the string has only uppercase and lowercase letters (a - z).
+
+*/
+
+#include <bits/stdc++.h>
+using namespace std;
+
+//Approach 1
+
+bool strigComp(string s)
+{
+}
+
+int main()
+{
+    string s1;
+    cin >> s1;
+    cout << strigComp(s1) << "\n";
+}
