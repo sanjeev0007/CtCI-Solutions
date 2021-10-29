1daeff38ad33efe899b0a57961b340a28d969c6f
Sanjeev Soni <44490954+sanjeev0007@users.noreply.github.com>
2021-10-29 21:03:23 +0530

    Create 3sum.cpp

---
+++ Arrays and Strings/3sum.cpp
@@ -0,0 +1,41 @@
+vector<vector<int>> threeSum(vector<int>& v) {
+        int n=v.size();
+        sort(v.begin(),v.end());
+        vector<vector<int>>ans;
+        
+        for(int i=0;i<n;++i)
+        {
+            int target=-v[i];
+            int front=i+1;
+            int back=n-1;
+            if(target<0)
+                break;
+            while(front<back)
+            {
+                int sum=v[front]+v[back];
+                if(sum>target)
+                    back--;
+                else if(sum<target)
+                    front++;
+                else
+                {
+                    vector<int>triplet(3,0);
+                    triplet[0]=v[i];
+                    triplet[1]=v[front];
+                    triplet[2]=v[back];
+                    ans.push_back(triplet);
+                    while(front<back && v[front]==triplet[1])
+                        front++;
+                    while(front<back && v[back]==triplet[2])
+                        back--;
+                    
+                }
+                while(i+1<v.size() && v[i]==v[i+1])
+                    i++;
+            }
+                
+                
+        }
+        return ans;
+    }
+    
